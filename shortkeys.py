cae6ef2fb7dab8bc38b3ffc1f29ed9ecc9f17286
Tobias <12374446+Vokatius@users.noreply.github.com>
2025-06-15 18:09:30 +0200

    Fixed AppOpener output

--- shortkeys.py
+++ shortkeys.py
@@ -1,6 +1,6 @@
 from scripts import hotkey_listener, system_tray, hotkey_registration as _
 import AppOpener
 
-AppOpener.mklist()
+AppOpener.mklist(output=False)
 hotkey_listener.start_hotkey_listener()
 system_tray.start_system_tray(lambda _: hotkey_listener.stop_hotkey_listener())
